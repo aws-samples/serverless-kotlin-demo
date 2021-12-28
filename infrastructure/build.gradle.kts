c6f52b8a68264568277d721b90ca4d31f3972228
Michael Brewer <michael.brewer@gyft.com>
2021-12-28 08:46:32 -0800

    refactor: no need for a class to have a main

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 dependencies {
-    implementation("software.amazon.awscdk:aws-cdk-lib:2.1.0")
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.2.0")
     implementation("software.amazon.awscdk:apigatewayv2-alpha:2.0.0-rc.24")
     implementation("software.amazon.awscdk:apigatewayv2-integrations-alpha:2.0.0-rc.24")
     implementation("software.constructs:constructs:10.0.15")
 }
 
 application {
-    mainClass.set("com.myorg.InfrastructureApp")
+    mainClass.set("com.myorg.InfrastructureAppKt")
 }
 
 tasks.named("run") {               
