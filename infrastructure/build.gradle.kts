f57222f38a079c930606ad411a700168c785b494
Michael Brewer <michael.brewer@gyft.com>
2021-12-28 21:40:01 -0800

    fix(deps): upgrade to pre-breaking 2.0.0-alpha.11

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 dependencies {
     implementation("software.amazon.awscdk:aws-cdk-lib:2.2.0")
-    implementation("software.amazon.awscdk:apigatewayv2-alpha:2.0.0-rc.24")
-    implementation("software.amazon.awscdk:apigatewayv2-integrations-alpha:2.0.0-rc.24")
+    implementation("software.amazon.awscdk:apigatewayv2-alpha:2.0.0-alpha.11")
+    implementation("software.amazon.awscdk:apigatewayv2-integrations-alpha:2.0.0-alpha.11")
     implementation("software.constructs:constructs:10.0.15")
 }
 
